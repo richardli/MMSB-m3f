779a51a1db0d6d294a68ba0dacd23b6f87787fd2
richardli <lizehang@gmail.com>
2014-03-01 17:48:02 -0800

    jagged cell for rating and evaluate variance

---
+++ m3f/fit_eval_var.m
@@ -0,0 +1,56 @@
+function [ana] = fit_eval_var(data, testData, opts)
+%
+% Function to evaluate fitting results by individual
+% Input: 
+%       data
+%       testdata
+%       trainPreds
+%       testPreds
+%
+% Output:
+%       ana - analysis
+%             ana.rmseByUser
+%             ana.rmseByItem
+%             ana.countByUser            
+%             ana.countByItem
+%             ana.varByUser
+%             ana.varByItem
+%       
+
+fprintf('Running fit_eval_val\n');
+
+trainrmseByUser = zeros(data.numUsers, 1);
+trainrmseByItem = zeros(data.numItems, 1);
+testrmseByUser = zeros(data.numUsers, 1);
+testrmseByItem = zeros(data.numItems, 1);
+
+[temp, countByUser] = cellfun(@size, data.exampsByUser);
+[temp, countByItem] = cellfun(@size, data.exampsByItem);
+varByUser = cellfun(@var, data.ratingsByUser);
+varByItem = cellfun(@var, data.ratingsByItem);
+
+for i = 1 : data.numUsers
+    trainrmseByUser(i) = evalPreds(data.ratingsByUser{i}, data.fitByUser{i}, 'rmse');
+    testrmseByUser(i) = evalPreds(testData.ratingsByUser{i}, testData.fitByUser{i}, 'rmse');
+end
+
+for i = 1 : data.numItems
+    trainrmseByItem(i) = evalPreds(data.ratingsByItem{i}, data.fitByItem{i}, 'rmse');
+    testrmseByItem(i) = evalPreds(testData.ratingsByItem{i}, testData.fitByItem{i}, 'rmse');
+end
+
+ana = struct('trainrmseByUser', trainrmseByUser, 'trainrmseByItem', trainrmseByItem, ...
+              'testrmseByUser', testrmseByUser, 'testrmseByItem', testrmseByItem, ...
+              'countByUser', countByUser, 'countByItem', countByItem, ... 
+               'varByUser', varByUser, 'varByItem', varByItem);
+
+itemmat = dataset({testrmseByItem, 'testRMSE'}, ... 
+                   {trainrmseByItem, 'trainRMSE'}, ...
+                   {varByItem, 'trainVariance'}, ...
+                   {countByUser}, 'count');          
+usermat = dataset({testrmseByUser, 'testRMSE'}, ... 
+                   {trainrmseByUser, 'trainRMSE'}, ...
+                   {varByUser, 'trainVariance'}, ...
+                   {countByUser}, 'count');          
+export(itemmat, 'file', opts.itemoutput);
+export(usermat, 'file', opts.useroutput);
---
+++ m3f/jaggedCellrating.m
@@ -0,0 +1,43 @@
+function [jCells] = jaggedCellrating(ids, ratings, numEntries)
+% JAGGEDCELL Jagged cell indexing of an array.
+% @author: richard
+%
+% Usage:
+%    [jCells] = jaggedCell(ids, numEntries)
+%
+% Inputs:
+%    ids - array of integer ids
+%    ratings - array of integer ratings
+%    numEntries - Maximum id accounted for in ids
+%
+% Outputs:
+%    jCells - Jagged cell array where the ith entry is an array of indices
+%             corresponding to each rating of i in ids
+
+% -----------------------------BEGIN CODE--------------------------------
+
+fprintf('Running jaggedCell\n');
+
+numExamples = length(ids);
+% Count the number of occurrences of each id in ids
+counts = accumarrayMex(ids, 1, [numEntries,1]);
+% Initialize jagged cell array
+jCells = cell(size(counts));
+for r = 1:numEntries
+    jCells{r} = zeros(1,counts(r), 'double');
+    counts(r) = 1;
+end
+% Populate jagged cell array with indices of entries
+% referencing each id in ids
+ratings = double(ratings);
+for e = 1:numExamples
+    r = ids(e);
+    % changed here @richard, if not all users are in the test data
+%    if r > numEntries
+%        continue;
+%    end    
+    jCells{r}(counts(r)) = ratings(e);
+    counts(r) = counts(r) + 1; 
+end
+
+% -----------------------------END OF CODE-------------------------------
